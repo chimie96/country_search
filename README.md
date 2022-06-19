e04a8dafd4b56f8451a3d1da0485fcc3f415323c
Nur Hidayah <93173249+chimie96@users.noreply.github.com>
2022-06-19 12:24:00 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# country_search
+This is a Junior-level JavaScript coding challenge. This website is developed by using simple basic HTML, CSS ad JavaScript.
+
+## Description
+This website entitled Search Country is about fetching data from RESTful API. The data are shown in the form of table which are name, capital, region and flag columns. The flag column is displayed by a small image that represents the particular country. Lastly, search bar at the top that filters down the displayed table by searching for name or capital columns.
+
+## Features
+1. Fetching data
+2. Table
+3. Images
+4. Filtering
+
+## How to run the website?
+1. Copy, paste and save all 3 coding files with the correct extension which are countries.html, script.js and style.css.
+2. Make sure all 3 files are saved in the same folder (e.g Desktop).
+3. Open the countries.html on the web browser (e.g Open with => Chrome).
+4. Finally, you are done!
+
+## References
+1. HTML source code: https://www.w3schools.com/html/default.asp
+2. HTML source code for go to top button: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+3. RESTful API source: restcountries.com
+4. API URL: https://restcountries.com/v3.1/all
+5. Display tabular data using JavaScript: https://www.geeksforgeeks.org/how-to-use-the-javascript-fetch-api-to-get-data/ 
+6. Search filter using JavaScript: https://insidethediv.com/javascript-filter-table-row-single-and-multiple-columns.
+7. CSS source code: https://www.w3schools.com/css/default.asp
+8. Fixed table header CSS coding 1: https://stackoverflow.com/questions/8423768/freeze-the-top-row-for-an-html-table-only-fixed-table-header-scrolling
+Fixed table header CSS coding 2: https://www.geeksforgeeks.org/how-to-make-bootstrap-table-with-sticky-table-head/
+9. Search box CSS: https://codepen.io/mihaeltomic/pen/vmwMdm
---
+++ fetch_filter.js
@@ -0,0 +1,87 @@
+// Title: Junior-level JavaScript coding challenge
+// Name: Nur Hidayah
+// Created by: 13/6-19/6/2022
+// Submitted by: 19/6/2022
+
+// fetch from RESTful API. source from: restcountries.com
+// display tabular data source code from: https://www.geeksforgeeks.org/how-to-use-the-javascript-fetch-api-to-get-data/
+
+// api url
+const api_url = "https://restcountries.com/v3.1/all";
+  
+// Defining async function
+async function getCountry(api_url) {
+    
+    // Storing response
+    const response = await fetch(api_url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    
+    console.log(data);
+    if (response) {
+        hideloader();
+    }
+    show(data);
+}
+
+// Calling that async function
+getCountry(api_url);
+  
+// Function to hide the loader
+function hideloader() {
+    document.getElementById('loading').style.display = 'none';
+}
+
+// Function to define innerHTML for HTML table
+function show(data) {
+
+    let table = 
+        `<thead class="header">
+        <tr>
+            <th class="header" scope="col">Name</th>
+            <th class="header" scope="col">Capital</th>
+            <th class="header" scope="col">Region</th>
+            <th class="header" scope="col">Flag</th>
+        </tr>
+        </thead>`;
+    
+    // Loop to access all rows 
+    for (let r of data) {
+        table += 
+            `<tbody>
+            <tr> 
+                <td>${r.name.common}</td>
+                <td>${r.capital}</td>
+                <td>${r.region}</td> 
+                <td><center><img src="${r.flags.png}" id="flag" width="50" /></center></td>          
+            </tr>
+            </tbody>`;
+    }
+
+    // Setting innerHTML as table variable
+    document.getElementById("countries").innerHTML = table;
+}
+
+// search filter name and capital. source code from: https://insidethediv.com/javascript-filter-table-row-single-and-multiple-columns
+function searchFilter(){
+    var filter, table, tr, i, name_column, capital_column, name_value, capital_value;
+    filter = document.getElementById("search").value.toLowerCase();
+    table = document.getElementById("countries");
+    tr = table.getElementsByTagName("tr");
+    for(i = 0; i < tr.length; i++){
+            name_column = tr[i].getElementsByTagName("td")[0];
+            capital_column = tr[i].getElementsByTagName("td")[1];
+            if(name_column && capital_column){
+                name_value = name_column.textContent || name_column.innerText;
+                capital_value = capital_column.textContent || capital_column.innerText;
+                name_value = name_value.toLowerCase();
+                capital_value = capital_value.toLowerCase();
+                if((name_value.indexOf(filter) > -1) || (capital_value.indexOf(filter) > -1)){
+                    tr[i].style.display = ""; // show
+                }else{
+                    tr[i].style.display = "none"; // hide
+                }
+            }
+        }
+}
